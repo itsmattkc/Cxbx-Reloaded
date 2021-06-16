d43acd6367e6b8723fe2405271da24c7d6ba0d12
PatrickvL <pvanlogchem@gmail.com>
2021-06-16 16:40:38 +0200

    Introduced Xbox version of D3DCUBEMAP_FACES

--- src/core/hle/D3D8/Direct3D9/Direct3D9.h
+++ src/core/hle/D3D8/Direct3D9/Direct3D9.h
@@ -834,11 +834,11 @@ xbox::bool_xt WINAPI EMUPATCH(D3DResource_IsBusy)
 xbox::void_xt WINAPI EMUPATCH(Lock2DSurface)
 (
     X_D3DPixelContainer *pPixelContainer,
-    D3DCUBEMAP_FACES     FaceType,
-    uint_xt                 Level,
+    X_D3DCUBEMAP_FACES   FaceType,
+    uint_xt              Level,
     D3DLOCKED_RECT      *pLockedRect,
     RECT                *pRect,
-    dword_xt                Flags
+    dword_xt             Flags
 );
 
 // ******************************************************************
@@ -941,11 +941,11 @@ xbox::void_xt WINAPI EMUPATCH(D3DVolumeTexture_LockBox)
 xbox::void_xt WINAPI EMUPATCH(D3DCubeTexture_LockRect)
 (
     X_D3DCubeTexture   *pThis,
-    D3DCUBEMAP_FACES    FaceType,
-    uint_xt                Level,
+    X_D3DCUBEMAP_FACES  FaceType,
+    uint_xt             Level,
     D3DLOCKED_RECT     *pLockedBox,
     CONST RECT         *pRect,
-    dword_xt               Flags
+    dword_xt            Flags
 );
 
 // ******************************************************************
@@ -2104,7 +2104,7 @@ xbox::void_xt WINAPI EMUPATCH(D3DDevice_GetPushBufferOffset)
 xbox::hresult_xt WINAPI EMUPATCH(D3DCubeTexture_GetCubeMapSurface)
 (
 	X_D3DCubeTexture*	pThis,
-	D3DCUBEMAP_FACES	FaceType,
+	X_D3DCUBEMAP_FACES	FaceType,
 	uint_xt				Level,
 	X_D3DSurface**		ppCubeMapSurface
 );
@@ -2115,7 +2115,7 @@ xbox::hresult_xt WINAPI EMUPATCH(D3DCubeTexture_GetCubeMapSurface)
 X_D3DSurface* WINAPI EMUPATCH(D3DCubeTexture_GetCubeMapSurface2)
 (
 	X_D3DCubeTexture*	pThis,
-	D3DCUBEMAP_FACES	FaceType,
+	X_D3DCUBEMAP_FACES	FaceType,
 	uint_xt				Level
 );
 
